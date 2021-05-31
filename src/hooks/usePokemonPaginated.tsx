222d4eeaad54879f0ba2cf2c623ad117ba71b462
maxrogers78 <maxrogerssepulveda@gmail.com>
2021-05-31 16:34:46 -0400

    http request with pagination created

--- src/hooks/usePokemonPaginated.tsx
+++ src/hooks/usePokemonPaginated.tsx
@@ -1,12 +1,32 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 // api
 import { pokemonApi } from "../api/pokemonApi";
+// interfaces
+import {
+  PokemonPaginatedResponse,
+  Result,
+  SimplePokemon,
+} from "../interfaces/pokemonInterfaces";
 
 export const usePokemonPaginated = () => {
+  const [simplePokemonList, setSimplePokemonList] = useState<SimplePokemon[]>(
+    []
+  );
+
   const nextPageUrl = useRef("https://pokeapi.co/api/v2/pokemon?limit=40");
 
   const loadPokemons = async () => {
-    const resp = await pokemonApi.get(nextPageUrl.current);
+    const resp = await pokemonApi.get<PokemonPaginatedResponse>(
+      nextPageUrl.current
+    );
+    nextPageUrl.current = resp.data.next;
+    mapPokemonList(resp.data.results);
+  };
+
+  const mapPokemonList = (pokemonList: Result[]) => {
+    pokemonList.map((pokemon) => {
+      console.log(pokemon.name);
+    });
   };
 
   useEffect(() => {
