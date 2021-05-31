eab8d7a4da2477e4e8cdd8ee0daf8d90c166ead2
maxrogers78 <maxrogerssepulveda@gmail.com>
2021-05-31 16:13:48 -0400

    title and backgroundImage for home screen added

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,22 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+// themes
+import { styles } from "../themes/appTheme";
 
 export const HomeScreen = () => {
+  const { top } = useSafeAreaInsets();
+
   return (
-    <View>
-      <Text>Home Screen</Text>
-    </View>
+    <>
+      <Image
+        source={require("../assets/pokebola.png")}
+        style={styles.pokeballBG}
+      />
+
+      <Text style={{ ...styles.title, ...styles.globalMargin, top: top + 20 }}>
+        Pokedex
+      </Text>
+    </>
   );
 };
